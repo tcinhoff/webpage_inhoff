2f7a470b4844b1f2812785353d2e984b08c4214b
timedrix <52511392+timedrix@users.noreply.github.com>
2021-06-29 18:16:35 +0200

    Erstellen der Gabentestseite mit fülltext

---
+++ src/Gabentest.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Typography} from "@material-ui/core";
+
+const Gabentest = () => {
+    return (
+        <div>
+            <Typography variant='h2' align='center'>
+                Gabentest
+            </Typography>
+        </div>
+    )
+}
+
+export default Gabentest
