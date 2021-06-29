431f5285f6c1e3b38d2b9dd866c33e6c6e194838
timedrix <52511392+timedrix@users.noreply.github.com>
2021-06-29 18:15:56 +0200

    Erstellen der Über Uns Seite mit Fülltext

---
+++ src/UeberUns.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Typography} from "@material-ui/core";
+
+const UeberUns = () => {
+    return (
+        <div>
+            <Typography variant='h2' align='center'>
+                Das sind wir:
+            </Typography>
+        </div>
+    )
+}
+
+export default UeberUns
